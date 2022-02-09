ca3eded9a94e5c211f6bb6e7f41650f7397aca6a
Hayden <haydenbryant.student@careerdevs.com>
2022-02-09 16:44:52 -0500

    got very basic version of console game working. Going to flush put more of the building of classes and game for console then will work on a gui.

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,13 +1,41 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Game {
     private Board board;
 
     public void game(){
         Board board = new Board(){};
         board.generateRandomBoard(4);
+        Scanner scan = new Scanner(System.in);
 
-        board.toString();
-        System.out.println(board);
+        ArrayList<String> ansList = new ArrayList<>();
+
+        System.out.println(board.toString());
+
+        for(int i = 0; i < 4; i++){
+            System.out.println("Guess color of peg " + (i + 1) + ". ");
+            String ans = scan.nextLine().toLowerCase();
+            ansList.add(ans);
+        }
+
+        Boolean correct = true;
+        for(int i = 0; i < 4; i++){
+            if(ansList.get(i).equals(board.getBoard().get(i).getColor())){
+                continue;
+            } else {
+                correct = false;
+                break;
+            }
+        }
+
+        if (correct){
+            System.out.println("Correct");
+        } else {
+            System.out.println("Wrong");
+        }
     }
 }
