5146ec601a573771ff90651168e0fb9ea7602adc
Hayden <haydenbryant.student@careerdevs.com>
2022-02-09 20:14:27 -0500

    seperated actions for game class

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -6,36 +6,46 @@ import java.util.Scanner;
 
 public class Game {
     private Board board;
+    Scanner scan = new Scanner(System.in);
 
     public void game(){
-        Board board = new Board(){};
-        board.generateRandomBoard(4);
-        Scanner scan = new Scanner(System.in);
+        Board ansBoard = new Board(){};
+        ansBoard.generateRandomBoard(4);
+//        System.out.println(ansBoard.toString());
 
-        ArrayList<String> ansList = new ArrayList<>();
+        Board guessBoard = guessBoard();
 
-        System.out.println(board.toString());
-
-        for(int i = 0; i < 4; i++){
-            System.out.println("Guess color of peg " + (i + 1) + ". ");
-            String ans = scan.nextLine().toLowerCase();
-            ansList.add(ans);
-        }
-
-        Boolean correct = true;
-        for(int i = 0; i < 4; i++){
-            if(ansList.get(i).equals(board.getBoard().get(i).getColor())){
-                continue;
-            } else {
-                correct = false;
-                break;
-            }
-        }
-
-        if (correct){
+        if (checkBoard(guessBoard, ansBoard)){
             System.out.println("Correct");
         } else {
             System.out.println("Wrong");
         }
+    }
+
+    public Board guessBoard(){
+        ArrayList<ColorPeg> ansList = new ArrayList<>();
+        Board guessBoard = new Board();
+
+        for(int i = 0; i < 4; i++){
+            System.out.println("Guess color of peg " + (i + 1) + ". ");
+            String ans = scan.nextLine().toLowerCase();
+            ColorPeg colorPeg = new ColorPeg(ans);
+            ansList.add(colorPeg);
+        }
+        guessBoard.setBoard(ansList);
+        return guessBoard;
+    }
+
+    public Boolean checkBoard(Board guessBoard, Board ansBoard){
+
+        for(int i = 0; i < 4; i++){
+            String guessColor = guessBoard.getBoard().get(i).getColor();
+            String ansColor = ansBoard.getBoard().get(i).getColor();
+            if(!guessColor.equals(ansColor)){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
